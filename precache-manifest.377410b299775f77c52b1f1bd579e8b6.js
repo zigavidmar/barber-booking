a94c843713e17b5a10d84a71121ff8d01e83e8a5
Ziga <zigavidmar96@gmail.com>
2020-06-17 18:42:02 +0200

    Updates

---
+++ precache-manifest.377410b299775f77c52b1f1bd579e8b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0620c41a17992607c5f9705c2330d5",
+    "url": "/barber-booking/index.html"
+  },
+  {
+    "revision": "55f85818761574a7c3d3",
+    "url": "/barber-booking/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c115fc0b15602965a12d",
+    "url": "/barber-booking/static/js/2.ba321933.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/barber-booking/static/js/2.ba321933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55f85818761574a7c3d3",
+    "url": "/barber-booking/static/js/main.8cfe6b66.chunk.js"
+  },
+  {
+    "revision": "f850afd95b864a31d59d",
+    "url": "/barber-booking/static/js/runtime-main.d0211a1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/barber-booking/static/media/logo.5d5d9eef.svg"
+  }
+]);
