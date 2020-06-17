0eb2f24850a93f9a4ce2e6f40e8dc67f9f633e74
Ziga <zigavidmar96@gmail.com>
2020-06-17 21:27:53 +0200

    end version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import AppointmentBooked from './components/AppointmentBooked';
 import AppointmentBooking from './components/AppointmentBooking';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, HashRouter} from 'react-router-dom'
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter basename="/">
       <Switch>
         <Route path="/" exact component={AppointmentBooking}/>
         <Route path="/barberbooked" component={AppointmentBooked}/>
       </Switch>
-    </Router>
+    </HashRouter>
   );
 }
 
